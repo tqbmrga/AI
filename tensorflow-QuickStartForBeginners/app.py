5bb735d4a8ee21dddc089d5bacd7b1a106fe8b28
Bao Trieu <tqb.mrga@gmail.com>
2020-04-01 02:16:03 +0700

    update

---
+++ tensorflow-QuickStartForBeginners/app.py
@@ -0,0 +1,56 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['Áo thun', 'Quần dài', 'Áo liền quần', 'Đầm', 'Áo khoác','Sandal', 'Áo sơ mi', 'Giày', 'Túi xách', 'Ủng']
+
+print(train_images.shape)
+
+print(len(train_labels))
+
+print(train_labels)
+
+print(test_images.shape)
+
+print(len(test_labels))
+
+# Tiền xử lý dữ liệu
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+# Thiết lập các layers
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
